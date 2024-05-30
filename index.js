aa453e0fa5b6d09bb76e27f6553ecdf56eafb29d
Negative121 <157050310+Negative121@users.noreply.github.com>
2024-05-30 17:54:30 +0200

    add files

---
+++ index.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-a6d2c-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const shoppingListEl = document.getElementById('shopping-list')
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+onValue(shoppingListInDB, function (snapshot) {
+
+    if (snapshot.exists()) {
+
+        let itemsArray = Object.entries(snapshot.val())
+
+        clearShoppingListEl()
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            appendItemToShoppingListEl(currentItem)
+        }
+    }
+    else {
+        shoppingListEl.innerText =  'No items here... yet'
+    }
+
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+addButtonEl.addEventListener("click", function () {
+    let inputValue = inputFieldEl.value
+
+    push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
+
+})
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let newEl = document.createElement('li')
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener('click', function () {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
+}
